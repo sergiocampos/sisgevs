4d1a61024bf080e8ba61467f76ddb9086978c791
sergiocampos <sergio.campos@dce.ufpb.br>
2021-12-03 11:13:58 -0300

    ajustes no urls

--- sisgevs/urls.py
+++ sisgevs/urls.py
@@ -80,6 +80,7 @@ urlpatterns = [
     path('ajax_index_aberto', views.ajax_index_aberto, name='ajax_index_aberto'),
     path('ajax_filtrar_index_aberto', views.ajax_filtrar_index_aberto, name='ajax_filtrar_index_aberto'),
     path('submit', views.ajax_exportar_index_aberto, name='ajax_exportar_index_aberto'),
+    path('ajax_gal', views.ajax_gal, name='ajax_gal'),
 
     
 ]
