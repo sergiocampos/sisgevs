15a4eb5927926a95de5160f71f1abcafd96ee106
Fillipe Vieira <fillipearaujo92@gmail.com>
2022-01-28 10:16:01 -0300

    Campos validados

--- core/views.py
+++ core/views.py
@@ -171,24 +171,9 @@ def localizar_paciente_data_coleta(request):
 
 @login_required(login_url='/login/')
 def set_localizar_paciente_data_coleta(request):
-	if request.method == "POST":
-		data = request.POST.get('data')
-		caso_all_result = CasoEsporotricose.objects.filter(data_coleta1__range=[data, data])
-	
-	if request.method == "GEt":
-		date = request.POST.get('data_coleta')
-		print(date)
-		caso_all_result = CasoEsporotricose.objects.filter(data_coleta1__range=[date, date])
-		data = []
-		for item in caso_all_result:
-			data.append([item.tipo_notificacao, item.agravo_doenca, item.codigo_cib10, item.data_notificacao, item.estado, item.municipio, item.codigo_ibge, item.data_primeiros_sintomas, item.unidade_saude, item.nome_paciente, item.data_nascimento_paciente, item.idade_paciente, item.sexo_paciente, item.paciente_gestante, item.raca_paciente, item.escolaridade_paciente, item.cartao_sus_paciente, item.nome_mae_paciente, item.cep_residencia, item.uf_residencia, item.municipio_residencia, item.bairro_residencia, item.codigo_ibge_residencia, item.rua_residencia, item.numero_residencia, item.complemento_residencia, item.distrito_residencia, item.ponto_referencia_residencia, item.telefone_residencia, item.zona_residencia, item.pais_residencia, item.data_investigacao, item.ocupacao, item.ambientes_frequentados, item.animais_que_teve_contato, item.natureza_contato_animais, item.relacao_animal_doente, item.exerce_atividade_contato_plantas, item.historico_contato_material, item.presenca_lesao_pele, item.natureza_lesao, item.local_lesao, item.diagnostico_forma_extrac_doenca, item.localizacao_forma_extrac_doenca, item.houve_coleta_material, item.data_coleta1, item.numero_gal1, item.data_coleta2, item.numero_gal2, item.data_coleta3, item.numero_gal3, item.resultado_isolamento, item.agente, item.histopatologia, item.data_resultado_exame1, item.descricao_exame_1, item.resultado_exame1, item.data_resultado_exame2, item.descricao_exame_2, item.resultado_exame2, item.data_resultado_exame3, item.descricao_exame_3, item.resultado_exame3, item.data_inicio_tratamento1, item.droga_administrada1, item.esquema_terapeutico1, item.data_inicio_tratamento2, item.droga_administrada2, item.esquema_terapeutico2, item.data_inicio_tratamento3, item.droga_administrada3, item.esquema_terapeutico3, item.hospitalizacao, item.data_internacao, item.data_da_alta, item.uf_hospitalizacao, item.municipio_hospitalizacao, item.codigo_ibge_hospitalizacao, item.nome_hospital_hospitalizacao, item.classificacao_final, item.criterio_confirmacao, item.caso_autoctone_municipio_residencia, item.uf_caso_autoctone, item.pais_caso_autoctone, item.municipio_caso_autoctone, item.codigo_ibge_caso_autoctone, item.distrito_caso_autoctone, item.bairro_caso_autoctone, item.area_provavel_infeccao_caso_autoctone, item.ambiente_infeccao_caso_autoctone, item.doenca_rel_trabalho_caso_autoctone, item.evolucao_caso, item.data_obito, item.data_encerramento, item.observacao, item.nome_investigador, item.funcao_investigador, item.email_investigador, item.telefone_investigador, item.conselho_classe_investigador, item.responsavel_pelas_informacoes_id, item.unidade_saude_outro, item.gerencia_id])
-		data = pd.DataFrame(data, columns=['Tipo de Notificação', 'Agravo/Doença', 'Codigo CID-10', 'Data de Notificação', 'Estado', 'Municipio', 'Codigo IBGE', 'Data dos Primeiros Sintomas', 'Unidade de Saúde', 'Nome do Paciente', 'Data de Nascimento do Paciente', 'Idade do Paciente', 'Sexo do Paciente', 'Paciente Gestante', 'Raça do Paciente', 'Escolaridade do Paciente', 'Cartão SUS do Paciente', 'Nome da Mãe do Paciente', 'CEP da Residência', 'UF da Residência', 'Municipio da Residência', 'Bairro da Residência', 'Codigo IBGE da Residência', 'Rua da Residência', 'Numero_da Residência', 'Complemento da Residência', 'Distrito da Residência', 'Ponto de Referência da Residência', 'Telefone da Residência', 'Zona da Residência', 'País da Residência', 'Data de Investigação', 'Ocupação', 'Ambientes Frequentados', 'Animais que Teve Contato', 'Natureza do Contato com os Animais', 'Relação com o Animal Doente', 'Exerce Atividade Contato Plantas', 'Histórico Contato Material', 'Presença de Lesão na Pele', 'Natureza da Lesão', 'Local da Lesão', 'Diagnóstico Forma Extrac Doença', 'Localização Forma Extrac Doença', 'Houve Coleta de Material', 'Data da Coleta 1', 'Numero Gal 1', 'Data da Coleta 2', 'Numero Gal 2', 'Data da Coleta 3', 'Numero Gal 3', 'Resultado de Isolamento', 'Agente', 'Histopatologia', 'Data do Restultado EXAME 1', 'Descrição EXAME 1', 'Resultado EXAME 1', 'Data do Resultado EXAME 2', 'Descrição EXAME 2', 'Resultado EXAME 2', 'Data do Resultado EXAME 3', 'Descrição EXAME 3', 'Resultado EXAME 3', 'Data de Início do Tratamento 1', 'Droga Administrada 1', 'Esquema Terapeutico 1', 'Data de Início do Tratamento 2', 'Droga Administrada 2', 'Esquema Terapeutico 2', 'Data de Início do Tratamento 3', 'Droga Administrada 3', 'Esquema Terapeutico 3', 'Hospitalizacão', 'Data de Internação', 'Data da Alta', 'UF da Hospitalização', 'Município da Hospitalização', 'Codigo IBGE da Hospitalização', 'Nome do Hospital da Hospitalização', 'Classificação Final', 'Critério de Confirmação', 'Caso Autoctone Municipio Residência', 'UF Caso Autoctone', 'País Caso Autoctone', 'Município Caso Autoctone', 'Codigo IBGE Caso Autoctone', 'Distrito Caso Autoctone', 'Bairro Caso Autoctone', 'Área Provável de Infecção Caso Autoctone', 'Ambiente Infecção Caso Autoctone', 'Doença Rel Trabalho Caso Autoctone', 'Evolução do Caso', 'Data do Óbito', 'Data de Encerramento', 'Observação', 'Nome do Investigador', 'Função do Investigador', 'Email do Investigador', 'Telefone do Investigador', 'Conselho Classe Investigador', 'Responsável Pelas Informações ID', 'Unidade de Saude Outro', 'Gerencia ID'])
-		response = HttpResponse(content_type = "text/csv")
-		response['Content-Disposition'] = 'attachment; filename=casos_esporotricose.csv'
-		data.to_csv(response, index=False)
-		print(data)
-		return response
-	
+	data = request.POST.get('data')
+	caso_all_result = CasoEsporotricose.objects.filter(data_coleta1__range=[data, data])
+		
 	return render(request, 'resultado_search_caso_data_coleta.html', {'caso_all_result':caso_all_result})
 
 @login_required(login_url='/login/')
@@ -444,9 +429,6 @@ def my_datas(request):
 		return render(request, 'my_datas.html', {'regs':regs})
 
 	elif request.user.funcao == 'municipal':
-		municipio_id_user = int(request.user.municipio_nome)
-		municipio_user = Municipio.objects.get(id=municipio_id_user)
-		
 		user_gerencia_operacional = request.user.gerencia_operacional
 		user_nucleo = request.user.nucleo
 		user_area_tecnica = request.user.area_tecnica
@@ -511,18 +493,19 @@ def criar_perfil_municipal(request):
 			gerencia_operacional = request.GET.get('gerencia_operacional')
 			nucleo = request.GET.get('nucleo')
 			area_tecnica = request.GET.get('area_tecnica')
+			telefone = request.GET.get('telefone')
+			
+			cpf = str(cpf).replace('.','').replace('-', '')			
+			telefone = telefone.replace('(','').replace(')','').replace('.','').replace('-','')			
+			nome = str(nome).upper()			
 
 			tamanho = 8
 			valores = string.ascii_letters + string.digits
 			senha = ''
 			for i in range(tamanho):
 				senha += choice(valores)
-
-			print(senha)
 			password = make_password(password=senha, salt=None, hasher='pbkdf2_sha256')
-			print(password)
-
-			
+						
 			User = get_user_model()
 			User.objects.create(
 				login=login,
