0a8438d744b18b1ba860b75e24728206409f42d3
Fillipe Vieira <fillipearaujo92@gmail.com>
2022-02-15 08:49:32 -0300

    Arquivos de insert SQL

---
+++ dados/sql_insert/public_core_macrorregiao_export_2022-02-15_082643.sql
@@ -0,0 +1,3 @@
+insert into "core_macrorregiao" ("id", "nome") values ('1', '1ª Macro');
+insert into "core_macrorregiao" ("id", "nome") values ('2', '2ª Macro');
+insert into "core_macrorregiao" ("id", "nome") values ('3', '3ª Macro');
