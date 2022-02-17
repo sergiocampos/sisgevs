bb725dabbf388f8287366d7b8d9eb77e1ec66187
Pedro Henrick <pedrohenrickqa@gmail.com>
2022-02-17 12:38:49 -0300

    conclui o script de deploy do docker

--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-echo 'Criando imagem docker'
+echo '#### Criando imagem docker ####'
 docker-compose up -d
-echo 'Inserindo dados'
-docker-compose exec db psql -U postgres -d postgres -f /insert/insert.sql
+sleep 10
+echo '#### Copiando o insert.sql ####'
+docker cp ./dados/sql_insert/insert_dados.sql sisgevs_db_1:/tmp
+echo '##### Inserindo dados ####'
+docker-compose exec db psql -U postgres -d postgres -f /tmp/insert_dados.sql
+echo '#### limpando arquivos ####'
+docker-compose exec db rm /tmp/insert_dados.sql
