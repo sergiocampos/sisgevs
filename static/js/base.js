c424bf331d11cb54c20f9b7662c77096d886c629
Pedro Henrick <pedrohenrickqa@gmail.com>
2022-03-03 13:48:52 -0300

    página de notificações e casos cancelados refeita

--- static/js/base.js
+++ static/js/base.js
@@ -6,3 +6,12 @@ $(document).ready(function(){
     $(".telefone").mask("(00) 00000-0000")
     $(".cpf").mask("000.000.000-00")
 })
+
+$(document).ready(function () {
+    $("#pesquisar-registros").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#table-registros tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
