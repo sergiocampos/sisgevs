60fa1d2971d38e4ca2171a106e2b8b6083e29830
Pedro Henrick <pedrohenrickqa@gmail.com>
2022-03-04 10:07:35 -0300

    correção js cancelar casos

--- static/js/base.js
+++ static/js/base.js
@@ -2,7 +2,7 @@ function goBack() {
     window.history.back();
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     $(".telefone").mask("(00) 00000-0000")
     $(".cpf").mask("000.000.000-00")
 })
@@ -14,4 +14,19 @@ $(document).ready(function () {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
     });
+});
+
+$('.cancelar_caso').on('click', function () {
+    var id = this.id;
+    var data = {
+        'id': id,
+        'url_redirect': 'my_datas'
+    }
+    $.ajax({
+        url: 'cancelar_caso_esporotricose',
+        data: data,
+        success: function (data) {
+            document.location.href = '/my_datas/'
+        }
+    })
 });
