54c9c247cdecd49aed8b4c03c2e49fe067ec813a
Fillipe Yuri Vieira Araujo <fillipevieira92@gmail.com>
2022-05-20 08:26:24 -0300

    criando base-views

---
+++ core/base_views.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+
+
+
+# TODO: Função responsável por retornar a pagina principal.
+
+
+# TODO: Função responsável por retornar a pagina dos dados do usuário.
+
+
+# TODO: Função responsável por retornar a pagina sub-admin.
+
+
+# TODO: Função responsável por retornar as notificações referente a pesquisa.
+
+
+# TODO: Função responsável por exportar dados.
+
+
+# TODO: Função responsável por cancelar um dado.
+
