52eda04c7c3689e7d694126ced633155839562fd
Fillipe Yuri Vieira Araujo <fillipevieira92@gmail.com>
2022-05-30 10:59:57 -0300

    Integrando o form de acidente de transito

--- _acidente_transito/views.py
+++ _acidente_transito/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+
+
+# Renderiza a pagina de formulário para criar um novo caso
+@login_required(login_url='/login')
+def criar_caso(request):
+    return render(request, 'criar_caso.html')
+
+
+# Capta o submit do formulario de criar caso.
+@login_required(login_url='/login')
+def set_criar_caso(request):
+    pass
