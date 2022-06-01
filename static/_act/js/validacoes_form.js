857b6775192d28bd4a58601b6b4144831ab5bd65
Fillipe Yuri Vieira Araujo <fillipevieira92@gmail.com>
2022-06-01 10:05:50 -0300

    Comentando JS

--- static/_act/js/validacoes_form.js
+++ static/_act/js/validacoes_form.js
@@ -10,4 +10,71 @@ function active_hospital(){
         opt.setAttribute("class","d-none")
     }
 }
+// VALIDAÇÕES
+// --------------------------------------------- INFORMAÇÕES GERAIS ------------------------------------------- //
+
+// Data do Acidente
+
+// Período do dia *
+
+// Em que dia da semana ocorreu o acidente *
+
+// O Acidente ocorreu em dia de feriado *
+
+// Município de Ocorrência do Acidente *
+
+// Endereço do local do Acidente: *
+
+// Tipo de Acidente: *
+
+// Tipo de veiculos envolvidos no acidente: *
+
+// O Paciente envolvido no acidente era: *
+
+// A vítima apresenta sinais de embriagues e/ou cosumo de bebidas alcoolicas: *
+
+
+// -------------------------------- SEVERIDADE DO ACIDENTE E QUANTIDADE DE VÍTIMAS ---------------------------- //
+
+// Houve vítimas fatais
+
+// Número de vítimas fatais envolvidas no acidente
+
+// Número de Feridos envolvidos no Acidente
+
+
+// ----------------------------------- EQUIPE ACIONADA PARA O LOCAL DO ACIDENTE ------------------------------- //
+
+// Quem foi responsável por prestar apoio no local: *
+
+
+// ------------------------------------------- INFORMACÕES DO PACIENTE ---------------------------------------- //
+
+// Nome do Paciente *
+
+// Idade do Paciente: *
+
+// Sexo: *
+
+// Nome da Mãe: *
+
+// Endereço do Paciente: *
+
+// Contato Telefônico: *
+
+
+// ------------------------------------- OUTRAS INFORMAÇÕES SOBRE O PACIENTE ---------------------------------- //
+
+// O Paciente Envolvido no Acidente *
+
+
+// -------------------------------------------- UNIDADE NOTIFICADORA ------------------------------------------ //
+
+// Nome da Instituição *
+
+// Nome do Secretário(a) Municipal de Saúde *
+
+// Cargo ou Função do Notificador *
+
+// Contato do Notificador *
 
