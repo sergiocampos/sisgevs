043838412b66be71b11c1ddfe87613b626e3ef4b
sergiocampos <sergio.campos@dce.ufpb.br>
2023-03-02 12:52:19 -0300

    aplicado modificações: ajustes no front aci criar caso, limitChar.js

--- static/base/js/limitChar.js
+++ static/base/js/limitChar.js
@@ -1,7 +1,7 @@
 function limitcarc(e, tipo) {
   var chr = String.fromCharCode(e.which);
   if (tipo == 'letr_acent'){
-      if ("áéíóúâêîôûãõ qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM".indexOf(chr) < 0)
+      if ("qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM".indexOf(chr) < 0)
         return false;
   }else if (tipo == 'num') {
       if ("0123456789".indexOf(chr) < 0)
@@ -10,7 +10,7 @@ function limitcarc(e, tipo) {
       if ("qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM".indexOf(chr) < 0)
         return false;
   } else if (tipo == 'letr_acent_num'){
-      if ("áéíóúâêîôûãõ qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM0123456789-,:;/|".indexOf(chr) < 0)
+      if ("qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM0123456789-,:;/|".indexOf(chr) < 0)
         return false;
   }
 }
